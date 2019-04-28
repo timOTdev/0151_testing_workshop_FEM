33d57d87833fae8a84f98db337ddaa618ffe0309
Timothy Hoang <timh1203@gmail.com>
2019-04-28 14:02:10 -0500

    Add thumb-war mock tests

--- other/whats-a-mock/__tests__/thumb-war.1.todo.js
+++ other/whats-a-mock/__tests__/thumb-war.1.todo.js
@@ -1,20 +1,119 @@
-// monkey-patching
+// // FIRST ITERATION
+// // monkey-patching
+// import thumbWar from '../thumb-war'
+// // import the utils module (see hint #1 at the bottom of the file)
+// import * as utils from '../utils'
+
+// test('returns winner', () => {
+//   // keep track of the original `getWinner` utility function (see hint #2)
+//   const originalGetWinner = utils.getWinner
+
+//   // overwrite the utils.getWinner function with
+//   // our own that always returns the second player (see hint #3)
+//   utils.getWinner = (p1, p2) => p2
+
+//   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
+//   // change this assertion to be more for a specific player
+//   // (like 'Kent C. Dodds', see hint #4):
+//   expect(winner).toBe('Kent C. Dodds')
+
+//   // restore the originalGetWinner function so other tests don't break
+//   // (see hint #5)
+//   utils.getWinner = originalGetWinner
+// })
+
+
+
+
+
+// // SECOND ITERATION
+// import thumbWar from '../thumb-war'
+// import * as utils from '../utils'
+
+// test('returns winner', () => {
+//   const originalGetWinner = utils.getWinner
+
+//   utils.getWinner = (...args) => {
+//     utils.getWinner.mock.calls.push(args)
+//     return args[1]
+//   }
+//   utils.getWinner.mock = { calls: [] }
+
+//   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
+//   expect(utils.getWinner.mock.calls).toHaveLength(2)
+//   utils.getWinner.mock.calls.forEach(args => {
+//     expect(args).toEqual(['Ken Wheeler', 'Kent C. Dodds'])
+//   })
+
+//   utils.getWinner = originalGetWinner
+// })
+
+
+
+
+
+// // THIRD ITERATION
+// import thumbWar from '../thumb-war'
+// import * as utils from '../utils'
+
+// test('returns winner', () => {
+//   const originalGetWinner = utils.getWinner
+
+//   utils.getWinner = jest.fn((p1, p2) => p2)
+
+//   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
+//   expect(winner).toBe('Kent C. Dodds')
+//   expect(utils.getWinner).toHaveBeenCalledTimes(2)
+//   utils.getWinner.mock.calls.forEach(args => {
+//     expect(args).toEqual(['Ken Wheeler', 'Kent C. Dodds'])
+//   })
+
+//   utils.getWinner = originalGetWinner
+// })
+
+
+
+
+
+// // FOURTH ITERATION
+// import thumbWar from '../thumb-war'
+// import * as utils from '../utils'
+
+// test('returns winner', () => {
+//   jest.spyOn(utils, 'getWinner')
+//   utils.getWinner = jest.fn((p1, p2) => p2)
+
+//   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
+//   expect(winner).toBe('Kent C. Dodds')
+//   expect(utils.getWinner).toHaveBeenCalledTimes(2)
+//   utils.getWinner.mock.calls.forEach(args => {
+//     expect(args).toEqual(['Ken Wheeler', 'Kent C. Dodds'])
+//   })
+// })
+
+
+
+
+
+// FIFTH ITERATION
 import thumbWar from '../thumb-war'
-// import the utils module (see hint #1 at the bottom of the file)
+import * as utilsMock from '../utils'
+
+jest.mock('../utils', () => {
+  return {
+    getWinner: jest.fn((p1, p2) => p2),
+  }
+})
 
 test('returns winner', () => {
-  // keep track of the original `getWinner` utility function (see hint #2)
-  // overwrite the utils.getWinner function with
-  // our own that always returns the second player (see hint #3)
-
   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
-  // change this assertion to be more for a specific player
-  // (like 'Kent C. Dodds', see hint #4):
-  expect(['Ken Wheeler', 'Kent C. Dodds'].includes(winner)).toBe(true)
-
-  // restore the originalGetWinner function so other tests don't break
-  // (see hint #5)
+  expect(winner).toBe('Kent C. Dodds')
+  expect(utilsMock.getWinner).toHaveBeenCalledTimes(2)
+  utilsMock.getWinner.mock.calls.forEach(args => {
+    expect(args).toEqual(['Ken Wheeler', 'Kent C. Dodds'])
+  })
 })
+
 
 /*
 
